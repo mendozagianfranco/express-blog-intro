4ebb665752b0f710cb91916037aad2059a979c68
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-03-26 13:54:43 +0100

    feat: add response post with images

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const post = require('./data/posts.js');
 const app = express();
 const port = 3000;
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     console.log('Siamo nella rotta Principale');
     res.send('Server del mio blog');
 });
 
+
+app.get('/bacheca', (req, res) => {
+    console.log('Siamo nella rotta Bacheca');
+    res.json(post);
+});
 
 app.listen(port, () => {
     console.log('Serve attivo in port: ' + port);
